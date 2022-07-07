851fa0d1897bfec206ac77cdbdf9d312d3a09f7c
Lyn <lynfinman@gmail.com>
2022-07-07 14:07:00 -0400

    commit tree 25d68a3d0ddd2816b93765b2df700e7941af0d03

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,22 @@
-# Write your code here.
+def badge_maker(name)
+  "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(all_names)
+  all_names.map { |name| badge_maker(name) }
+end
+
+def assign_rooms(speakers)
+  room_assignments = []
+  room_num = 1
+  speakers.each do |speaker|
+    room_assignments << "Hello, #{speaker}! You'll be assigned to room #{room_num}!"
+    (room_num += 1) > 7 ? room_num = 1 : room_num
+  end
+  room_assignments
+end
+
+def printer(group)
+  group.each { |person| puts badge_maker(person) }
+  assign_rooms(group).each { |assignment| puts assignment }
+end
